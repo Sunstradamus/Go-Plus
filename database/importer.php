267fdab01f6478da021791d854e44988caf45454
Andy <mrandysun+github@gmail.com>
2014-02-12 02:39:47 -0800

    Updated SQL dumper to be stable

--- database/importer.php
+++ database/importer.php
@@ -7,58 +7,92 @@ $cont = fread($fh, filesize('dmp.dat'));
 fclose($fh);
 $arr = unserialize($cont);
 
-$obj = $arr[0];
-
 $mysqli = new mysqli($GLOBALS["SQLHOST"], $GLOBALS["SQLUSR"], $GLOBALS["SQLPWD"], $GLOBALS["SQLDB"]);
 if($mysqli->connect_errno) {
 	echo "Failed to connect: (" . $mysqli->connect_errno . ") ". $mysqli->connect_error;
 }
 
-$sql = "INSERT INTO `course` (`subject`, `level`, `title`, `career`, `wqb`, `units`, `prereq`, `desc`) SELECT '" . $obj->getDept() . "', '" . $obj->getLevel() . "', '" . $obj->getName() . "', '" . $obj->getCareer() . "', '" . $obj->getWQB() . "', '" . $obj->getUnits() . "', '" . $obj->getPrereq() . "', '" . $obj->getDesc() . "' FROM DUAL WHERE NOT EXISTS (SELECT * FROM `course` WHERE subject='" . $obj->getDept() . "' AND level='" . $obj->getLevel() . "')";
-
-if(!$mysqli->query($sql)){
-	echo $mysqli->errno . $mysqli->error;
-}
-
-$course_id = $mysqli->insert_id;
-if($mysqli->insert_id != 0){
-	echo "Success: Course ID - " . $mysqli->insert_id;
-} else {
-	echo "Duplicate entry";
-}
-
-if($obj->getLectures() != NULL){
-	$lec = $obj->getLectures();
-
-	$sql = "INSERT INTO `section`(`course_id`, `id`, `type`, `section`, `term`) VALUES ('" . $mysqli->insert_id . "', '" . $lec[0]['id'] . "', 'LEC', '" . $lec[0]['section'] . "', '" . $lec[0]['term'] . "')";
+for($i = 0; $i < count($arr); $i++){
+	$obj = $arr[$i];
+	$sql = "INSERT INTO `course` (`subject`, `level`, `title`, `career`, `wqb`, `units`, `prereq`, `desc`) SELECT '" . $obj->getDept() . "', '" . $obj->getLevel() . "', '" . $obj->getName() . "', '" . $obj->getCareer() . "', '" . $obj->getWQB() . "', '" . $obj->getUnits() . "', '" . $obj->getPrereq() . "', '" . $obj->getDesc() . "' FROM DUAL WHERE NOT EXISTS (SELECT * FROM `course` WHERE subject='" . $obj->getDept() . "' AND level='" . $obj->getLevel() . "')";
 
 	if(!$mysqli->query($sql)){
 		echo $mysqli->errno . $mysqli->error;
 	}
-	echo "Successfully inserted LEC";
 
-	$instructor_id = 1;
-	if(substr_count($lec[0]['prof'][0], "Tbd") == 1 || substr_count($lec[0]['prof'][0], "TBA") == 1){
-		$sql = "INSERT INTO `instructor`(`name`) VALUES ('" . $lec[0]['prof'][0] . "')";
-		if(!$mysqli->query($sql)){
-			echo $mysqli->errno . $mysqli->error;
+	if($mysqli->insert_id != 0){
+		echo "Success: Course ID - " . $mysqli->insert_id ."<br/>";
+		$course_id = $mysqli->insert_id;
+	} else {
+		echo "Duplicate entry, retrieving Course ID - ";
+		$sql = "SELECT `id` FROM `course` WHERE subject ='" . $obj->getDept() . "' AND level = '" . $obj->getLevel() . "'";
+		if($res = $mysqli->query($sql)){
+			$row = $res->fetch_assoc();
+			$course_id = $row['id'];
+			echo $course_id . "<br/>";
+		} else {
+			die($mysqli->errno . $mysqli->error);
 		}
-		$instructor_id = $mysqli->insert_id;
-		echo "Success: Instructor ID - " . $instructor_id;
 	}
 
-	$sql = "INSERT INTO `sectionComponent`(`section_id`, `day`, `instructor`, `timeslot`, `campus`, `room`, `dates`) VALUES ('" . $lec[0]['id'] . "', '" . $lec[0]['day'][0] . "', '" . $instructor_id . "', '0', '" . $lec[0]['campus'][0] . "', '" . $lec[0]['room'][0] . "', '" . $lec[0]['dates'][0] . "')";
+	if($obj->getLectures() != NULL){
+		$lec = $obj->getLectures();
+	
+		for($j = 0; $j < count($lec); $j++){
+			$sql = "INSERT IGNORE INTO `section`(`course_id`, `id`, `type`, `section`, `term`) VALUES ('" . $course_id . "', '" . $lec[$j]['id'] . "', 'LEC', '" . $lec[$j]['section'] . "', '" . $lec[$j]['term'][0] . "')";
 
-	if(!$mysqli->query($sql)){
-		echo $mysqli->errno . $mysqli->error;
+			if(!$mysqli->query($sql)){
+				die ($mysqli->errno . $mysqli->error);
+			}
+			if($mysqli->affected_rows != 0){
+				echo "Successfully inserted LEC<br/>";
+			} else {
+				echo "Duplicate entry, skipping section<br/>";
+			}
+			for($k = 0; $k < count($lec[$j]['term']); $k++){
+				$instructor_id = 1;
+				if(substr_count($lec[$j]['prof'][$k], ".") == 0 || substr_count($lec[$j]['prof'][$k], "TBA") == 0 || substr_count($lec[$j]['prof'][$k], "Staff") == 0){
+					$sql = "INSERT INTO `instructor`(`name`) SELECT '" . $lec[$j]['prof'][$k] . "' FROM DUAL WHERE NOT EXISTS (SELECT * FROM `instructor` WHERE name='" . $lec[$j]['prof'][$k] . "')";
+					if(!$mysqli->query($sql)){
+						die ($mysqli->errno . $mysqli->error);
+					}
+					if($mysqli->insert_id != 0){
+						$instructor_id = $mysqli->insert_id;
+						echo "Success: Instructor ID - " . $instructor_id . "<br/>";
+					} else {
+						echo "Duplicate instructor, retrieving ID - ";
+						$sql = "SELECT `id` FROM `instructor` WHERE name='" . $lec[$j]['prof'][$k] . "'";
+						if($res = $mysqli->query($sql)){
+							$row = $res->fetch_assoc();
+							$instructor_id = $row['id'];
+							echo $instructor_id . "<br/>";
+						} else {
+							die($mysqli->errno . $mysqli->error);
+						}
+					}
+				} elseif (substr_count($lec[$j]['prof'][$k], "Exam") == 1) {
+					$instructor_id = 2;
+				}
+
+				$sql = "INSERT INTO `sectionComponent`(`section_id`, `day`, `instructor`, `timeslot`, `campus`, `room`, `dates`) SELECT '" . $lec[$j]['id'] . "', '" . $lec[$j]['day'][$k] . "', '" . $instructor_id . "', '0', '" . $lec[$j]['campus'][$k] . "', '" . $lec[$j]['room'][$k] . "', '" . $lec[$j]['dates'][$k] . "' FROM DUAL WHERE NOT EXISTS (SELECT * FROM `sectionComponent` WHERE section_id='".$lec[$j]['id']."' AND day='".$lec[$j]['day'][$k]."' AND instructor='".$instructor_id."' AND timeslot='0' AND room='".$lec[$j]['room'][$k]."' AND dates='".$lec[$j]['dates'][$k]."')";
+
+				if(!$mysqli->query($sql)){
+					echo $mysqli->errno . $mysqli->error;
+				}
+				if($mysqli->affected_rows != 0){
+					echo "Successfully updated LEC components<br/>";
+				} else {
+					echo "Duplicate LEC component, skipping<br/>";
+				}
+			}
+		}
+	} elseif ($obj->getTutorials() != NULL){
+		$tut = $obj->getTutorials();
+	} elseif ($obj->getLabs() != NULL){
+		$lab = $obj->getLabs();
+	} else {
+		$sec = $obj->getSections();
 	}
-	echo "Successfully inserted LEC components";
-} elseif ($obj->getTutorials() != NULL){
-	$tut = $obj->getTutorials();
-} elseif ($obj->getLabs() != NULL){
-	$lab = $obj->getLabs();
-} else {
-	$sec = $obj->getSections();
 }
 
 $mysqli->close();
